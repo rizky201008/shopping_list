c8166ea842595a5c8a716371273a82fadd79fec3
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-12-09 15:02:40 +0700

    Sending a POST Request to the Backend

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
@@ -19,14 +21,29 @@ class _NewItemState extends State<NewItem> {
 
   void _saveItem() {
     if (_formKey.currentState!.validate()) _formKey.currentState!.save();
-    Navigator.of(context).pop(
-      GroceryItem(
-        id: DateTime.now().toString(),
-        name: _enteredName,
-        quantity: _enteredQuantity,
-        category: _selectedCategory,
+    final url = Uri.https("shopping-list-468bd-default-rtdb.firebaseio.com",
+        'shopping-list.json');
+    http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode(
+        {
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selectedCategory.title,
+        },
       ),
     );
+    // Navigator.of(context).pop(
+    //   GroceryItem(
+    //     id: DateTime.now().toString(),
+    //     name: _enteredName,
+    //     quantity: _enteredQuantity,
+    //     category: _selectedCategory,
+    //   ),
+    // );
   }
 
   @override
@@ -103,7 +120,7 @@ class _NewItemState extends State<NewItem> {
                                 const SizedBox(
                                   width: 6,
                                 ),
-                                Text(category.value.name)
+                                Text(category.value.title)
                               ],
                             ),
                           )
