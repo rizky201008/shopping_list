bb650cc6d5e1e9e0e60d6b226b50c7e42f87d92a
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-12-09 15:15:44 +0700

    Working with the Request & Waiting for the Response

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:http/http.dart' as http;
-import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -19,11 +18,11 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) _formKey.currentState!.save();
     final url = Uri.https("shopping-list-468bd-default-rtdb.firebaseio.com",
         'shopping-list.json');
-    http.post(
+    final response = await http.post(
       url,
       headers: {
         'Content-Type': 'application/json',
@@ -36,7 +35,15 @@ class _NewItemState extends State<NewItem> {
         },
       ),
     );
-    // Navigator.of(context).pop(
+
+    print(response.body);
+
+    if (!context.mounted) {
+      return;
+    }
+
+    Navigator.of(context).pop();
+
     //   GroceryItem(
     //     id: DateTime.now().toString(),
     //     name: _enteredName,
