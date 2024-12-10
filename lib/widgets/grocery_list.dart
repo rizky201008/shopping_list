ad56ffed268bae1fc42287a712f58f9d5ce2af89
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-12-10 15:55:52 +0700

    Handling the "No Data" Case

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -37,6 +37,13 @@ class _GroceryListState extends State<GroceryList> {
       });
     }
 
+    if (response.body == 'null') {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
+
     final Map<String, dynamic> listData = await json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
@@ -72,11 +79,17 @@ class _GroceryListState extends State<GroceryList> {
     }
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     final url = Uri.https("shopping-list-468bd-default-rtdb.firebaseio.com",
         'shopping-list/${item.id}.json');
 
-    http.delete(url);
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
 
     setState(() {
       _groceryItems.remove(item);
